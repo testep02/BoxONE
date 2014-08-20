53fc9090fa1c5c96fceb373cd337696c305966cd
Travis Estep <testep02@gmail.com>
2014-08-19 22:10:13 -0400

    Revert "Revert "Initial BoxONE checkin""
    This reverts commit 0fe8f163ca4ede79a8571b53b68c8493def0c738.

---
+++ BoxONE.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BoxONE.Domain.Entities;
+
+namespace BoxONE.Domain.Concrete
+{
+    class EFDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ BoxONE.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoxONE.WebUI.MvcApplication" Language="C#" %>
